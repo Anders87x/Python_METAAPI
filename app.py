fe4bbdc0078612c42f3c055b4fac40308bc6b4c5
AnderCode <davisanderson87@gmail.com>
2024-02-20 13:10:52 -0500

    a6

--- app.py
+++ app.py
@@ -4,38 +4,34 @@ app = Flask(__name__)
 
 TOKEN_ANDERCODE = "ANDERCODEPYTHONAPIMETA"
 
-def verificar_token(req):
-    try:
-        token = req.args.get('hub.verify_token')
-        challenge = req.args.get('hub.challenge')
-
-        if challenge and token and token == TOKEN_ANDERCODE:
-            return jsonify({'hub.challenge': challenge})
-        else:
-            return jsonify({'error': 'Invalid token'})
-
-    except Exception as e:
-        return jsonify({'error': f'Error during token verification: {str(e)}'})
-
-def recibir_mensajes(req):
-    try:
-        data = req.json
-
-        # Aquí puedes procesar el mensaje de WhatsApp según tus necesidades
-        # Por ejemplo, puedes imprimir el contenido del mensaje:
-        print(data['messages'][0]['body'])
-
-        return jsonify({'message': 'EVENT_RECEIVED'})
-
-    except Exception as e:
-        return jsonify({'error': f'Error processing message: {str(e)}'})
-
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'GET':
         return verificar_token(request)
     elif request.method == 'POST':
         return recibir_mensajes(request)
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_ANDERCODE:
+        return jsonify({'hub.challenge': challenge})
+    else:
+        return jsonify({'error': 'Invalid token'}), 401
+
+def recibir_mensajes(req):
+    data = req.get_json()
+
+    if 'entry' in data:
+        for entry in data['entry']:
+            if 'changes' in entry:
+                for change in entry['changes']:
+                    if 'value' in change:
+                        # Procesar el mensaje según tus necesidades
+                        print(change['value'])
+
+    return jsonify({'message': 'EVENT_RECEIVED'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
