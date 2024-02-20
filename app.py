0705cb8a4fd2b78c909e15bf5d00d6d69fd08a8a
AnderCode <davisanderson87@gmail.com>
2024-02-20 12:37:39 -0500

    a3

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ app = Flask(__name__)
 
 TOKEN_ANDERCODE = "ANDERCODEPYTHONAPIMETA"
 
+def verificar_token(req):
+    try:
+        token = req.args.get('hub_verify_token')
+        challenge = req.args.get('hub_challenge')
+
+        if challenge and token and token == TOKEN_ANDERCODE:
+            return challenge
+        else:
+            return jsonify({'error': 'Invalid token'})
+
+    except Exception as e:
+        return jsonify({'error': f'Error during token verification: {str(e)}'})
+
 def recibir_mensajes(req):
     try:
         entry = req.json['entry'][0]
@@ -17,10 +30,14 @@ def recibir_mensajes(req):
     except Exception as e:
         return jsonify({'error': f'Error processing message: {str(e)}'})
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    response = recibir_mensajes(request)
-    return response
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        response = recibir_mensajes(request)
+        return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
